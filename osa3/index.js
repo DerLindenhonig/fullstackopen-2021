271ec4e3b2bb4c7ea31d7387bf1ee69223266514
annarae <anna.raevskaia@metropolia.fi>
2021-11-09 10:06:16 +0200

    Tehtävä 3.1

--- osa3/index.js
+++ osa3/index.js
@@ -3,63 +3,60 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
+let persons = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2020-01-10T17:30:31.098Z",
-        important: true
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2020-01-10T18:39:34.091Z",
-        important: false
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2020-01-10T19:20:14.298Z",
-        important: true
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 3
     }
 ]
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
 // yksittäisen resurssin haku
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+    const person = persons.find(note => note.id === id)
 
-    if (note) {
-        response.json(note)
+    if (person) {
+        response.json(person)
     } else {
         response.status(404).end()
     }
 })
 
 // poistaan note:
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
 
 // lisääminen
 const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
         : 0
     return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
     const body = request.body
 
     if (!body.content) {
@@ -68,16 +65,16 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const person = {
         content: body.content,
         important: body.important || false,
         date: new Date(),
         id: generateId(),
     }
 
-    notes = notes.concat(note)
+    persons = persons.concat(person)
 
-    response.json(note)
+    response.json(person)
 })
 
 const PORT = 3001
