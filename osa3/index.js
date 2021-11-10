0f397c16e4fa9ca48625ab22c94c4d97ef28be0d
DerLindenhonig <79135600+DerLindenhonig@users.noreply.github.com>
2021-11-10 11:45:35 +0200

    Update index.js

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const morgan = require("morgan");
+const app = express();
+const cors = require('cors')
 
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
+    next();
+};
+
+app.use(express.static('build'))
 app.use(express.json())
+app.use(requestLogger);
+app.use(morgan('tiny'));
+app.use(cors());
+
+morgan.token("body", request => JSON.stringify(request.body));
 
 let persons = [
     {
@@ -20,12 +36,9 @@ let persons = [
         "id": 3
     }
 ]
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
 // yksittäisen resurssin haku
@@ -55,23 +68,16 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 // lisääminen
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (body.name === undefined) {
+    if (!body.name) {
         return response.status(400).json({
             error: "name missing"
         });
     }
 
-    if (body.number === undefined) {
+    if (!body.number) {
         return response.status(400).json({
             error: "number missing"
         });
@@ -80,21 +86,21 @@ app.post('/api/persons', (request, response) => {
     const person = {
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * 100000),
+        id: Math.floor(Math.random() * 100),
     }
 
-    person
-        .save()
-        .then(savedPerson => savedPerson.toJSON())
-        .catch(error => {
-            return response.status(400).json({
-                error: "name must be unique"
-            });
-        });
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-
 })
