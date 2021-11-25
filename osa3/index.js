fd85233abfd0fe4b9c1594aa9e1ec19654fb63b1
DerLindenhonig <79135600+DerLindenhonig@users.noreply.github.com>
2021-11-25 23:40:19 +0200

    Korjauksia, viimeistely

--- osa3/index.js
+++ osa3/index.js
@@ -1,60 +1,58 @@
-require('dotenv').config()
-const express = require('express');
-const morgan = require("morgan");
-const app = express();
+const express = require('express')
+const morgan = require("morgan")
+const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const Person = require('./models/person')
 
 const requestLogger = (request, response, next) => {
-    console.log("Method:", request.method);
-    console.log("Path:  ", request.path);
-    console.log("Body:  ", request.body);
-    console.log("---");
-    next();
-};
+    console.log("Method:", request.method)
+    console.log("Path:  ", request.path)
+    console.log("Body:  ", request.body)
+    console.log("---")
+    next()
+}
 
 app.use(express.static('build'))
 app.use(express.json())
-app.use(requestLogger);
-app.use(morgan('tiny'));
-app.use(cors());
+app.use(requestLogger)
+app.use(morgan('tiny'))
+app.use(cors())
 
-morgan.token("body", request => JSON.stringify(request.body));
+morgan.token("body", request => JSON.stringify(request.body))
 
 app.get('/info', (request, response) => {
     Person.find({}).then(persons => {
-        persons.map(person => person.toJSON());
-        response.send(`<p>Phonebook has info for ${persons.length} people.</p>
-    <p>${new Date()}</p>`)
+        response.send(`<p>Phonebook has info for ${persons.length} people. ${new Date()}</p>`)
     })
 })
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
-        response.json(persons.map(person => person.toJSON()));
+        response.json(persons.map(person => person.toJSON()))
     })
 })
 
 // yksittäisen resurssin haku
 app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person.toJSON())
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+      .then(person => {
+          if (person) {
+              response.json(person)
+          } else {
+              response.status(404).end()
+          }
+      })
+      .catch(error => next(error))
 })
 
 // poistaan note:
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end();
-        })
-        .catch(error => next(error));
+      .then(result => {
+          response.status(204).end();
+      })
+      .catch(error => next(error));
 })
 
 // lisääminen
@@ -76,18 +74,18 @@ app.post('/api/persons', (request, response, next) => {
     const person = new Person({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * 1000),
+        id: Math.floor(Math.floor(Math.random() * (10000000 - 10000)))
     })
 
     person
-        .save()
-        .then(savedPerson => {
-            response.send(savedPerson.toJSON())
-        })
-        .then(savedAndFormattedNote => {
-            response.json(savedAndFormattedNote)
-        })
-        .catch(error => next(error));
+      .save()
+      .then(savedPerson => {
+          response.send(savedPerson.toJSON())
+      })
+      .then(savedAndFormattedNote => {
+          response.json(savedAndFormattedNote)
+      })
+      .catch(error => next(error));
 })
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -99,16 +97,15 @@ app.put("/api/persons/:id", (request, response, next) => {
     };
 
     Person.findByIdAndUpdate(request.params.id, person, { new: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson.toJSON());
-        })
-        .catch(error => next(error));
+      .then(updatedPerson => {
+          response.json(updatedPerson);
+      })
+      .catch(error => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 // olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
 
@@ -120,10 +117,8 @@ const errorHandler = (error, request, response, next) => {
     } else if (error.name === 'ValidationError') {
         return response.status(400).send({ error: error.message })
     }
-
     next(error)
 }
-
 // virheellisten pyyntöjen käsittely
 app.use(errorHandler)
 
