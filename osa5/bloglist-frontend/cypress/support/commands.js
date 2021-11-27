6583252df8e66958ca68693790cd2910d1c837e3
DerLindenhonig <79135600+DerLindenhonig@users.noreply.github.com>
2021-11-27 14:40:53 +0200

    Cypress test "blogin poistaminen" on korjattu

--- osa5/bloglist-frontend/cypress/support/commands.js
+++ osa5/bloglist-frontend/cypress/support/commands.js
@@ -25,24 +25,20 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', ({ username, password }) => {
-  cy.request('POST', 'http://localhost:3001/api/login', {
-    username,
-    password,
-  }).then((response) => {
+  cy.request('POST', 'http://localhost:3001/api/login', { username, password,})
+    .then((response) => {
     localStorage.setItem('loggedBlogAppUser', JSON.stringify(response.body))
+
     cy.visit('http://localhost:3000')
   })
 })
 
 Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
-  const content = likes ? { title, author, url, likes } : { title, author, url }
   cy.request({
     url: 'http://localhost:3001/api/blogs',
     method: 'POST',
-    body: content,
-    headers: {
-      Authorization: `bearer ${JSON.parse(localStorage.getItem('loggedBlogAppUser')).token}`,
-    },
+    body: { title: title, author: author, url: url, likes: likes },
+    headers: { Authorization: `bearer ${JSON.parse(localStorage.getItem('loggedBlogAppUser')).token}`, },
   })
   cy.visit('http://localhost:3000')
 })
