9a86709a13110831ca5f548e0887728a06b8d05c
DerLindenhonig <79135600+DerLindenhonig@users.noreply.github.com>
2021-12-20 15:55:09 +0200

    Update App.js

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -57,16 +57,16 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, resetContent] = useField('text')
-  const [author, resetAuthor] = useField('text')
-  const [info, resetInfo] = useField('text')
-
   const history = useHistory();
 
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
   const resetInputs = () => {
-    resetContent();
-    resetAuthor();
-    resetInfo();
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   const handleSubmit = (e) => {
@@ -104,94 +104,94 @@ const CreateNew = (props) => {
 }
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: '1'
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: '2'
-    }
-  ])
+const [anecdotes, setAnecdotes] = useState([
+  {
+    content: 'If it hurts, do it more often',
+    author: 'Jez Humble',
+    info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+    votes: 0,
+    id: '1'
+  },
+  {
+    content: 'Premature optimization is the root of all evil',
+    author: 'Donald Knuth',
+    info: 'http://wiki.c2.com/?PrematureOptimization',
+    votes: 0,
+    id: '2'
+  }
+])
 
-  const [notification, setNotification] = useState('')
+const [notification, setNotification] = useState('')
 
-  const addNew = (anecdote) => {
-    anecdote.id = (Math.random() * 10000).toFixed(0)
-    setAnecdotes(anecdotes.concat(anecdote))
+const addNew = (anecdote) => {
+  anecdote.id = (Math.random() * 10000).toFixed(0)
+  setAnecdotes(anecdotes.concat(anecdote))
 
-    setNotification(`new anecdote ${anecdote.content} was created`)
-    setTimeout(() => {
-      setNotification("")
-    }, 10000);
+  setNotification(`new anecdote ${anecdote.content} was created`)
+  setTimeout(() => {
+    setNotification("")
+  }, 10000);
+}
+
+const anecdoteById = (id) =>
+  anecdotes.find(a => a.id === id)
+
+const vote = (id) => {
+  const anecdote = anecdoteById(id)
+
+  const voted = {
+    ...anecdote,
+    votes: anecdote.votes + 1
   }
 
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
+  setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+}
 
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
+const match = useRouteMatch('/:id')
+const anecdote = match
+  ? anecdotes.find(anecdote => anecdote.id === match.params.id)
+  : null
 
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
-  }
-
-  const match = useRouteMatch('/:id')
-  const anecdote = match
-    ? anecdotes.find(anecdote => anecdote.id === match.params.id)
-    : null
-
-  const Nav = () => {
-    const padding = {
-      paddingRight: 5
-    }
-
-    return (
-      <div>
-        <Link style={padding} to="/">all anecdotes</Link>
-        <Link style={padding} to="/create">create new</Link>
-        <Link style={padding} to="/about">about</Link>
-      </div>
-    )
+const Nav = () => {
+  const padding = {
+    paddingRight: 5
   }
 
   return (
     <div>
-      <h1>Anecdotes</h1>
-      <Nav/>
-      {notification}
-      <Switch>
-        <Route path='/about'>
-          <About/>
-        </Route>
-        <Route path='/create'>
-          <CreateNew addNew={addNew}/>
-        </Route>
-        <Route path='/:id'>
-          <Anecdote
-            anecdote={anecdote}
-            vote={vote}
-          />
-        </Route>
-        <Route path='/'>
-          <AnecdoteList anecdotes={anecdotes}/>
-        </Route>
-      </Switch>
-      <br/>
-      <Footer/>
+      <Link style={padding} to="/">all anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
+}
+
+return (
+  <div>
+    <h1>Anecdotes</h1>
+    <Nav/>
+    {notification}
+    <Switch>
+      <Route path='/about'>
+        <About/>
+      </Route>
+      <Route path='/create'>
+        <CreateNew addNew={addNew}/>
+      </Route>
+      <Route path='/:id'>
+        <Anecdote
+          anecdote={anecdote}
+          vote={vote}
+        />
+      </Route>
+      <Route path='/'>
+        <AnecdoteList anecdotes={anecdotes}/>
+      </Route>
+    </Switch>
+    <br/>
+    <Footer/>
+  </div>
+)
 }
 
 export default App;
